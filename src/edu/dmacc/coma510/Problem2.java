eae1f67f19d9a25c0de1740dc2a0232e64f76e8b
eelin9763 <eelin@dmacc.edu>
2019-10-11 17:36:51 -0500

    Finished with 1 and 2

--- src/edu/dmacc/coma510/Problem2.java
+++ src/edu/dmacc/coma510/Problem2.java
@@ -2,14 +2,14 @@ package edu.dmacc.coma510;
 
 public class Problem2 {
     public static void main(String[] args) {
-//        int wholeFeetHeight = 5;
-//        int remainingInchesHeight = 9;
-//        int centimetersInInch = 2.54;
-//        int inchesInFoot = 12;
-//
-//        int heightInInches = (wholeFeetHeight * inchesInFoot) + remainingInchesHeight;
-//        int heightInCentimeters = heightInInches * centimetersInInch;
-//
-//        System.out.println(heightInCentimeters); //Expect 175.26
+        int wholeFeetHeight = 5;
+        int remainingInchesHeight = 9;
+        double centimetersInInch = 2.54;
+        int inchesInFoot = 12;
+
+        int heightInInches = (wholeFeetHeight * inchesInFoot) + remainingInchesHeight;
+        double heightInCentimeters = heightInInches * centimetersInInch;
+
+        System.out.println(heightInCentimeters); //Expect 175.26
     }
 }
